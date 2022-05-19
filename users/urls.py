635c356e1ad2b90436100c44944d3c1847a33f22
Sabbir <bsse1014@iit.du.ac.bd>
2022-05-19 16:34:48 +0600

    token_obtainpar,token refresh,token verify done

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from .views import *
 
 urlpatterns = [
     path('api/user-register/', register),
-    path('api/verify_email/',verify_email)
+    path('api/verify_email/',verify_email),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
