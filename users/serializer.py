c7ef90442755a9bc51df1e5cb9f99380b8ba00e1
Sabbir <bsse1014@iit.du.ac.bd>
2022-05-23 15:06:38 +0600

    password validator add in registration

--- users/serializer.py
+++ users/serializer.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+import django.contrib.auth.password_validation as validators
+
 from .models import *
 from rest_framework_friendly_errors.mixins import FriendlyErrorMessagesMixin
 
@@ -11,6 +13,7 @@ class RegistrationSerializer(FriendlyErrorMessagesMixin, serializers.Serializer)
     email = serializers.EmailField()
     name = serializers.CharField(max_length=100)
     password= serializers.CharField()
+
 
 class OtpVerificationSerializer(FriendlyErrorMessagesMixin, serializers.Serializer):
     email = serializers.EmailField()
